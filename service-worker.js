b8231e47c413e03e133cac888e714623f1447e81
bharatvyb <bharatvyb@outlook.com>
2024-08-31 14:42:49 +0530

    fixed it as PWA

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+// Basic service worker setup
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('my-cache').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/style.css',
+          '/record.js',
+          '/settings.js',
+          '/monthly.js',
+          '/total.js',
+          '/icons/icon-192x192.png',
+          '/icons/icon-512x512.png'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
