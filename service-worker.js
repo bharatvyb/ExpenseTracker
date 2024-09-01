a3e1019774145b2eb4b704dc4c38ad144348212b
bharatvyb <bharatvyb@outlook.com>
2024-09-01 09:29:40 +0530

    trying to fix app issue in iphone

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,29 @@
 // Basic service worker setup
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('my-cache').then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/style.css',
-          '/record.js',
-          '/settings.js',
-          '/monthly.js',
-          '/total.js',
-          '/icons/icon-192x192.png',
-          '/icons/icon-512x512.png'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-  });
+  event.waitUntil(
+    caches.open('my-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/record.css',
+        '/record.js',
+        '/settings.css',
+        '/settings.js',
+        '/monthly.css',
+        '/monthly.js',
+        '/total.css',
+        '/total.js',
+        '/icons/icon-192x192.png',
+        '/icons/icon-512x512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
