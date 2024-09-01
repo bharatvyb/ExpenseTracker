5e8de236f5631fdb5a13600856e22d5b4bc4d28f
bharatvyb <bharatvyb@outlook.com>
2024-09-01 13:05:24 +0530

    slight modifications

--- service-worker.js
+++ service-worker.js
@@ -4,17 +4,23 @@ self.addEventListener('install', function(event) {
     caches.open('my-cache').then(function(cache) {
       return cache.addAll([
         '/',
-        '/index.html',
-        '/record.css',
-        '/record.js',
-        '/settings.css',
-        '/settings.js',
-        '/monthly.css',
-        '/monthly.js',
-        '/total.css',
-        '/total.js',
-        '/icons/icon-192x192.png',
-        '/icons/icon-512x512.png'
+        'index.html',
+        'record.css',
+        'record.js',
+        'settings.css',
+        'settings.js',
+        'monthly.css',
+        'monthly.js',
+        'total.css',
+        'total.js',
+        'manage_categories.css',
+        'manage_categories.js',
+        'manage_categories.html',
+        'manage_payment_methods.css',
+        'manage_payment_methods.js',
+        'manage_payment_methods.html',
+        'icons/icon-192x192.png',
+        'icons/icon-512x512.png'
       ]);
     })
   );
